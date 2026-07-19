ffb264b2dcf84e873300ccda157b0bec104f1afa
Augustinas Malinauskas <augustinasmal@gmail.com>
2026-07-19 07:50:44 +0100

    Remove Matrix deployment stack

--- matrix/postgres/init/01-create-bridge-dbs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-for db in mautrix_whatsapp mautrix_telegram; do
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-SQL
-	SELECT 'CREATE DATABASE $db'
-	WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec
-	SQL
-done
